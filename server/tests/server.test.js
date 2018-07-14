262582cb0df8ed33001d2be413adf0781366e8e5
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-07-14 14:11:54 +0200

    added tests for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb')
 const expect = require('expect')
 const request = require('supertest')
 
@@ -5,8 +6,10 @@ const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }]
 
@@ -68,4 +71,32 @@ describe('GET /todos', () => {
       })
       .end(done)
   })
+})
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text)
+      })
+      .end(done)
+  })
+
+  it('should return 404 if id not found', (done) => {
+    const id = new ObjectID().toHexString()
+    request(app)
+      .get(`/todos/${id}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done)
+  })
+
 })
